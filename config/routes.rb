35c7e27ef629c9f1572e7f29fc44e552d18b2c96
Jorge L Pioquinto <129575128+Jorge-Pioquinto@users.noreply.github.com>
2023-10-26 16:53:00 +0000

    Added liked photos route

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  get ":username/liked" => "users#liked", as: :liked
+
   get "/:username" => "users#show", as: :user
 end
